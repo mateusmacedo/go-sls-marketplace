32f8cfe63d0fc23d503ba5a2feb8ab5b43d0c7e9
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-19 15:01:59 -0300

    feat: :sparkles: add get_all_products lambda function
    introduces AWS Lambda function to retrieve all products from DynamoDB table.

---
+++ internal/catalog/infrastructure/http/aws/adapter/get_all_products.go
@@ -0,0 +1,84 @@
+package adapter
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"github.com/mateusmacedo/go-sls-marketplace/internal/catalog/application"
+	infrahttp "github.com/mateusmacedo/go-sls-marketplace/internal/catalog/infrastructure/http"
+	pkghttp "github.com/mateusmacedo/go-sls-marketplace/pkg/infrastructure/http"
+)
+
+type GetAllProductsResponse struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
+}
+
+type LambdaGetAllProductsAdapter struct {
+	service application.GetAllProductsUseCase
+	db      *dynamodb.DynamoDB
+	table   string
+}
+
+func NewLambdaGetAllProductsAdapter(service application.GetAllProductsUseCase) *LambdaGetAllProductsAdapter {
+	sess := session.Must(session.NewSession())
+	db := dynamodb.New(sess)
+
+	return &LambdaGetAllProductsAdapter{
+		service: service,
+		db:      db,
+		table:   os.Getenv("PRODUCTS_TABLE"),
+	}
+}
+
+func (a *LambdaGetAllProductsAdapter) Handle(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if request.HTTPMethod != http.MethodGet {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusMethodNotAllowed,
+			Body:       pkghttp.ErrHttpMethodNotAllowed.Error(),
+		}, nil
+	}
+
+	products, err := a.service.Execute()
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: infrahttp.HttpError[err],
+			Body:       err.Error(),
+		}, nil
+	}
+
+	response := make([]GetAllProductsResponse, len(products))
+	for i, product := range products {
+		response[i] = GetAllProductsResponse{
+			ID:          string(product.ID),
+			Name:        product.Name,
+			Description: product.Description,
+			Price:       product.Price,
+			CreatedAt:   product.CreatedAt,
+			UpdatedAt:   product.UpdatedAt,
+		}
+	}
+
+	responseBody, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(responseBody),
+	}, nil
+}
