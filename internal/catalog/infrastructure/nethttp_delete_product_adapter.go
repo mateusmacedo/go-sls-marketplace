cf5f95d9410d2f0ccd251941f8ebc459ee6d99dd
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-19 05:41:08 -0300

    refactor: :hammer: improve error handling consistency
    Standardized error responses by utilizing error message texts directly from error objects. Enhanced HTTP status responses by mapping error types dynamically. Updated corresponding tests accordingly for consistency.

--- internal/catalog/infrastructure/nethttp_delete_product_adapter.go
+++ internal/catalog/infrastructure/nethttp_delete_product_adapter.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/mateusmacedo/go-sls-marketplace/internal/catalog/application"
-	"github.com/mateusmacedo/go-sls-marketplace/internal/catalog/domain"
 )
 
 type DeleteProductRequest struct {
@@ -24,30 +23,21 @@ func NewNetHTTPDeleteProductAdapter(useCase application.DeleteProductUseCase) *N
 
 func (a *NetHTTPDeleteProductAdapter) Handle(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodDelete {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, ErrHttpMethodNotAllowed.Error(), http.StatusMethodNotAllowed)
 		return
 	}
 
 	var req DeleteProductRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	input := application.DeleteProductInput{
+	err := a.useCase.Execute(application.DeleteProductInput{
 		ID: req.ID,
-	}
-
-	err := a.useCase.Execute(input)
+	})
 	if err != nil {
-		switch err {
-		case domain.ErrNotFoundProduct:
-			http.Error(w, "Product not found", http.StatusNotFound)
-		case domain.ErrInvalidProductID:
-			http.Error(w, "Invalid product ID", http.StatusBadRequest)
-		default:
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-		}
+		http.Error(w, err.Error(), HttpError[err])
 		return
 	}
 
