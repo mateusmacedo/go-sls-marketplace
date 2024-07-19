27d2588fd9256773684ae1a18ce1606e9ee66398
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-19 11:09:16 -0300

    feat: :sparkles: add dynamodb product repository
    introduce dynamodb adapter and repository for products. supports CRUD operations integrating AWS.

---
+++ internal/catalog/infrastructure/db/dynamodb/adapter/dynamodb_api.go
@@ -0,0 +1,14 @@
+package adapter
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+type DynamoDBAPI interface {
+	PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error)
+	GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
+	Scan(ctx context.Context, params *dynamodb.ScanInput, optFns ...func(*dynamodb.Options)) (*dynamodb.ScanOutput, error)
+	DeleteItem(ctx context.Context, params *dynamodb.DeleteItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.DeleteItemOutput, error)
+}
---
+++ internal/catalog/infrastructure/db/dynamodb/adapter/product_repository.go
@@ -0,0 +1,109 @@
+package adapter
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/mateusmacedo/go-sls-marketplace/internal/catalog/domain"
+)
+
+func initDB() *dynamodb.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	return dynamodb.NewFromConfig(cfg)
+}
+
+type dynamoDbProductRepository struct {
+	DB        DynamoDBAPI
+	TableName string
+}
+
+func NewDynamoDbProductRepository(db DynamoDBAPI, tableName string) domain.ProductRepository {
+	return &dynamoDbProductRepository{DB: db, TableName: tableName}
+}
+
+func (r *dynamoDbProductRepository) Save(product *domain.Product) error {
+	entity, err := NewProductEntityFromDomain(product)
+	if err != nil {
+		return err
+	}
+
+	item, err := attributevalue.MarshalMap(entity)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.DB.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: &r.TableName,
+		Item:      item,
+	})
+	return err
+}
+
+func (r *dynamoDbProductRepository) Find(id domain.ProductID) (*domain.Product, error) {
+	result, err := r.DB.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: &r.TableName,
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: string(id)},
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Item == nil {
+		return nil, fmt.Errorf("product not found")
+	}
+
+	var entity DynamoDbProductEntity
+	err = attributevalue.UnmarshalMap(result.Item, &entity)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity.ToDomain()
+}
+
+func (r *dynamoDbProductRepository) FindAll() ([]*domain.Product, error) {
+	result, err := r.DB.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName: &r.TableName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var entities []*DynamoDbProductEntity
+	err = attributevalue.UnmarshalListOfMaps(result.Items, &entities)
+	if err != nil {
+		return nil, err
+	}
+
+	products := make([]*domain.Product, 0, len(entities))
+	for _, entity := range entities {
+		product, _ := entity.ToDomain()
+		products = append(products, product)
+	}
+
+	return products, nil
+}
+
+func (r *dynamoDbProductRepository) Delete(id domain.ProductID) error {
+	_, err := r.DB.DeleteItem(context.TODO(), &dynamodb.DeleteItemInput{
+		TableName: &r.TableName,
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: string(id)},
+		},
+	})
+	return err
+}
