48e6bf08b92333114677c6665551b2e9a6c58b36
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-21 05:33:24 -0300

    refactor: :hammer: implement service locator pattern
    Replaced manual dependency injection with service locator pattern for better scalability and maintainability.

--- cmd/catalog/gorilla/mux/main.go
+++ cmd/catalog/gorilla/mux/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/mateusmacedo/go-sls-marketplace/internal/catalog/domain"
 	dbadapter "github.com/mateusmacedo/go-sls-marketplace/internal/catalog/infrastructure/db/gorm/adapter"
 	httpadapter "github.com/mateusmacedo/go-sls-marketplace/internal/catalog/infrastructure/http/net/adapter"
+	pkgapplication "github.com/mateusmacedo/go-sls-marketplace/pkg/application"
 	pkghttp "github.com/mateusmacedo/go-sls-marketplace/pkg/infrastructure/http"
 )
 
@@ -26,36 +27,103 @@ func InitializeServer() (*mux.Router, error) {
 		return nil, err
 	}
 
-	productFindRepo := dbadapter.NewGormProductFindRepository(dbConn)
-	productSaveRepo := dbadapter.NewGormProductSaveRepository(dbConn)
-	adderProductService := domain.NewProductAdder(productFindRepo, productSaveRepo)
-	addProductUseCase := application.NewAddProductUseCase(adderProductService)
+	serviceLocator := pkgapplication.NewSimpleServiceLocator()
+	factory := pkgapplication.NewFactory(serviceLocator)
 
-	productDeleteRepo := dbadapter.NewGormProductDeleteRepository(dbConn)
-	deleterProductService := domain.NewProductDeleter(productFindRepo, productDeleteRepo)
-	deleteProductUseCase := application.NewDeleteProductUseCase(deleterProductService)
+	serviceLocator.Register("dbConn", dbConn)
 
-	productFindAllRepo := dbadapter.NewGormProductFindAllRepository(dbConn)
-	fiderAllProductService := domain.NewAllProductFinder(productFindAllRepo)
-	getAllProductsUseCase := application.NewGetAllProductsUseCase(fiderAllProductService)
+	// Repositories
+	factory.RegisterRecipe("ProductSaveRepository", pkgapplication.Recipe{
+		Dependencies: []string{"dbConn"},
+		Factory:      dbadapter.CreateProductSaveRepository,
+	})
+	factory.RegisterRecipe("ProductFindRepository", pkgapplication.Recipe{
+		Dependencies: []string{"dbConn"},
+		Factory:      dbadapter.CreateProductFindRepository,
+	})
+	factory.RegisterRecipe("ProductFindAllRepository", pkgapplication.Recipe{
+		Dependencies: []string{"dbConn"},
+		Factory:      dbadapter.CreateProductFindAllRepository,
+	})
+	factory.RegisterRecipe("ProductDeleteRepository", pkgapplication.Recipe{
+		Dependencies: []string{"dbConn"},
+		Factory:      dbadapter.CreateProductDeleteRepository,
+	})
+	// Domain Services
+	factory.RegisterRecipe("ProductAdder", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFindRepository", "ProductSaveRepository"},
+		Factory:      domain.CreateProductAdder,
+	})
+	factory.RegisterRecipe("ProductDeleter", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFindRepository", "ProductDeleteRepository"},
+		Factory:      domain.CreateProductDeleter,
+	})
+	factory.RegisterRecipe("ProductFinder", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFindRepository"},
+		Factory:      domain.CreateProductFinder,
+	})
+	factory.RegisterRecipe("AllProductFinder", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFindAllRepository"},
+		Factory:      domain.CreateAllProductFinder,
+	})
+	// Application Use Cases
+	factory.RegisterRecipe("AddProductUseCase", pkgapplication.Recipe{
+		Dependencies: []string{"ProductAdder"},
+		Factory:      application.CreateAddProductUseCase,
+	})
+	factory.RegisterRecipe("DeleteProductUseCase", pkgapplication.Recipe{
+		Dependencies: []string{"ProductDeleter"},
+		Factory:      application.CreateDeleteProductUseCase,
+	})
+	factory.RegisterRecipe("GetAllProductsUseCase", pkgapplication.Recipe{
+		Dependencies: []string{"AllProductFinder"},
+		Factory:      application.CreateGetAllProductsUseCase,
+	})
+	factory.RegisterRecipe("GetProductUseCase", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFinder"},
+		Factory:      application.CreateGetProductUseCase,
+	})
+	factory.RegisterRecipe("UpdateProductUseCase", pkgapplication.Recipe{
+		Dependencies: []string{"ProductUpdater"},
+		Factory:      application.CreateUpdateProductUseCase,
+	})
 
-	finderProductService := domain.NewProductFinder(productFindRepo)
-	getProductUseCase := application.NewGetProductUseCase(finderProductService)
+	addProductUseCase, err := factory.Create("AddProductUseCase")
+	if err != nil {
+		panic(err)
+	}
 
-	updaterProductService := domain.NewProductUpdater(productFindRepo, productSaveRepo)
-	updateProductUseCase := application.NewUpdateProductUseCase(updaterProductService)
+	deleteProductUseCase, err := factory.Create("DeleteProductUseCase")
+	if err != nil {
+		panic(err)
+	}
+
+	getAllProductsUseCase, err := factory.Create("GetAllProductsUseCase")
+	if err != nil {
+		panic(err)
+	}
+
+	getProductUseCase, err := factory.Create("GetProductUseCase")
+	if err != nil {
+		panic(err)
+	}
+
+	updateProductUseCase, err := factory.Create("UpdateProductUseCase")
+	if err != nil {
+		panic(err)
+	}
 
 	postHttpMethodGuard := pkghttp.NewHttpMethodGuard([]string{http.MethodPost})
 
 	addProductHandler := httpadapter.NewNetHTTPAddProductAdapter(
-		httpadapter.WithService(addProductUseCase),
+		httpadapter.WithService(addProductUseCase.(application.AddProductUseCase)),
 		httpadapter.WithMethodGuard(postHttpMethodGuard),
 	)
 
-	deleteProductHandler := httpadapter.NewNetHTTPDeleteProductAdapter(deleteProductUseCase)
-	getAllProductsHandler := httpadapter.NewNetHTTPGetAllProductsAdapter(getAllProductsUseCase)
-	getProductHandler := httpadapter.NewNetHTTPGetProductAdapter(getProductUseCase)
-	updateProductHandler := httpadapter.NewNetHTTPUpdateProductAdapter(updateProductUseCase)
+	deleteProductHandler := httpadapter.NewNetHTTPDeleteProductAdapter(deleteProductUseCase.(application.DeleteProductUseCase))
+	getAllProductsHandler := httpadapter.NewNetHTTPGetAllProductsAdapter(getAllProductsUseCase.(application.GetAllProductsUseCase))
+	getProductHandler := httpadapter.NewNetHTTPGetProductAdapter(getProductUseCase.(application.GetProductUseCase))
+	updateProductHandler := httpadapter.NewNetHTTPUpdateProductAdapter(updateProductUseCase.(application.UpdateProductUseCase))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/products", addProductHandler.Handle).Methods(http.MethodPost)
