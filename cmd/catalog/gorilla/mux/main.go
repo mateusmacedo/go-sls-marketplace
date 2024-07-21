1a73492be5480902bfb993ab30668801f30f0b02
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-21 05:51:03 -0300

    feat: :sparkles: integrate repositories and domain services
    Added repository and domain service registrations to the service locator.
    Ensures proper initialization and availability of these components at startup.

--- cmd/catalog/gorilla/mux/main.go
+++ cmd/catalog/gorilla/mux/main.go
@@ -32,7 +32,6 @@ func InitializeServer() (*mux.Router, error) {
 
 	serviceLocator.Register("dbConn", dbConn)
 
-	// Repositories
 	factory.RegisterRecipe("ProductSaveRepository", pkgapplication.Recipe{
 		Dependencies: []string{"dbConn"},
 		Factory:      dbadapter.CreateProductSaveRepository,
@@ -49,6 +48,27 @@ func InitializeServer() (*mux.Router, error) {
 		Dependencies: []string{"dbConn"},
 		Factory:      dbadapter.CreateProductDeleteRepository,
 	})
+	productSaveRepository, err := factory.Create("ProductSaveRepository")
+	if err != nil {
+		panic(err)
+	}
+	productFindRepository, err := factory.Create("ProductFindRepository")
+	if err != nil {
+		panic(err)
+	}
+	productFindAllRepository, err := factory.Create("ProductFindAllRepository")
+	if err != nil {
+		panic(err)
+	}
+	productDeleteRepository, err := factory.Create("ProductDeleteRepository")
+	if err != nil {
+		panic(err)
+	}
+	serviceLocator.Register("ProductSaveRepository", productSaveRepository)
+	serviceLocator.Register("ProductFindRepository", productFindRepository)
+	serviceLocator.Register("ProductFindAllRepository", productFindAllRepository)
+	serviceLocator.Register("ProductDeleteRepository", productDeleteRepository)
+
 	// Domain Services
 	factory.RegisterRecipe("ProductAdder", pkgapplication.Recipe{
 		Dependencies: []string{"ProductFindRepository", "ProductSaveRepository"},
@@ -66,6 +86,36 @@ func InitializeServer() (*mux.Router, error) {
 		Dependencies: []string{"ProductFindAllRepository"},
 		Factory:      domain.CreateAllProductFinder,
 	})
+	factory.RegisterRecipe("ProductUpdater", pkgapplication.Recipe{
+		Dependencies: []string{"ProductFindRepository", "ProductSaveRepository"},
+		Factory:      domain.CreateProductUpdater,
+	})
+	productAdder, err := factory.Create("ProductAdder")
+	if err != nil {
+		panic(err)
+	}
+	productDeleter, err := factory.Create("ProductDeleter")
+	if err != nil {
+		panic(err)
+	}
+	productFinder, err := factory.Create("ProductFinder")
+	if err != nil {
+		panic(err)
+	}
+	allProductFinder, err := factory.Create("AllProductFinder")
+	if err != nil {
+		panic(err)
+	}
+	productUpdater, err := factory.Create("ProductUpdater")
+	if err != nil {
+		panic(err)
+	}
+	serviceLocator.Register("ProductAdder", productAdder)
+	serviceLocator.Register("ProductDeleter", productDeleter)
+	serviceLocator.Register("ProductFinder", productFinder)
+	serviceLocator.Register("AllProductFinder", allProductFinder)
+	serviceLocator.Register("ProductUpdater", productUpdater)
+
 	// Application Use Cases
 	factory.RegisterRecipe("AddProductUseCase", pkgapplication.Recipe{
 		Dependencies: []string{"ProductAdder"},
@@ -87,7 +137,6 @@ func InitializeServer() (*mux.Router, error) {
 		Dependencies: []string{"ProductUpdater"},
 		Factory:      application.CreateUpdateProductUseCase,
 	})
-
 	addProductUseCase, err := factory.Create("AddProductUseCase")
 	if err != nil {
 		panic(err)
@@ -113,13 +162,12 @@ func InitializeServer() (*mux.Router, error) {
 		panic(err)
 	}
 
+	// HTTP Adapters
 	postHttpMethodGuard := pkghttp.NewHttpMethodGuard([]string{http.MethodPost})
-
 	addProductHandler := httpadapter.NewNetHTTPAddProductAdapter(
 		httpadapter.WithService(addProductUseCase.(application.AddProductUseCase)),
 		httpadapter.WithMethodGuard(postHttpMethodGuard),
 	)
-
 	deleteProductHandler := httpadapter.NewNetHTTPDeleteProductAdapter(deleteProductUseCase.(application.DeleteProductUseCase))
 	getAllProductsHandler := httpadapter.NewNetHTTPGetAllProductsAdapter(getAllProductsUseCase.(application.GetAllProductsUseCase))
 	getProductHandler := httpadapter.NewNetHTTPGetProductAdapter(getProductUseCase.(application.GetProductUseCase))
