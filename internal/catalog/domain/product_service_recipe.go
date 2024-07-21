432f56ba996ec6ba7bc954b7a287bf56942f95d2
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-21 05:48:49 -0300

    refactor: :hammer: standardize dependency keys in factory
    Updated dependency keys to camel case in product service factories. Enhances consistency and readability.

--- internal/catalog/domain/product_service_recipe.go
+++ internal/catalog/domain/product_service_recipe.go
@@ -1,34 +1,34 @@
 package domain
 
 func CreateProductAdder(dependencies map[string]interface{}) (interface{}, error) {
-	findRepository := dependencies["findRepository"].(ProductFindRepository)
-	saveRepository := dependencies["saveRepository"].(ProductSaveRepository)
+	findRepository := dependencies["ProductFindRepository"].(ProductFindRepository)
+	saveRepository := dependencies["ProductSaveRepository"].(ProductSaveRepository)
 
 	return NewProductAdder(findRepository, saveRepository), nil
 }
 
 func CreateProductDeleter(dependencies map[string]interface{}) (interface{}, error) {
-	findRepository := dependencies["findRepository"].(ProductFindRepository)
-	deleteRepository := dependencies["deleteRepository"].(ProductDeleteRepository)
+	findRepository := dependencies["ProductFindRepository"].(ProductFindRepository)
+	deleteRepository := dependencies["ProductDeleteRepository"].(ProductDeleteRepository)
 
 	return NewProductDeleter(findRepository, deleteRepository), nil
 }
 
 func CreateProductFinder(dependencies map[string]interface{}) (interface{}, error) {
-	findRepository := dependencies["findRepository"].(ProductFindRepository)
+	findRepository := dependencies["ProductFindRepository"].(ProductFindRepository)
 
 	return NewProductFinder(findRepository), nil
 }
 
 func CreateAllProductFinder(dependencies map[string]interface{}) (interface{}, error) {
-	findAllRepository := dependencies["findAllRepository"].(ProductFindAllRepository)
+	findAllRepository := dependencies["ProductFindAllRepository"].(ProductFindAllRepository)
 
 	return NewAllProductFinder(findAllRepository), nil
 }
 
 func CreateProductUpdater(dependencies map[string]interface{}) (interface{}, error) {
-	findRepository := dependencies["findRepository"].(ProductFindRepository)
-	saveRepository := dependencies["saveRepository"].(ProductSaveRepository)
+	findRepository := dependencies["ProductFindRepository"].(ProductFindRepository)
+	saveRepository := dependencies["ProductSaveRepository"].(ProductSaveRepository)
 
 	return NewProductUpdater(findRepository, saveRepository), nil
 }
